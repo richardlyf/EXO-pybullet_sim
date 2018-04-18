47619e29aa5f37760c4983d2c54dd89fdb76bd7b
Richard Lin <yifengl@stanford.com>
2018-04-18 16:58:08 -0700

    The simulation file should be able to read from a list and display it now

--- pybullet_sim.py
+++ pybullet_sim.py
@@ -7,7 +7,9 @@ import time
 
 physicsClient = p.connect(p.GUI)
 p.setGravity(0, 0, -9.8)
-armID = p.loadURDF("./exo-urdf/01-exo_arm.urdf", useFixedBase=True)
+
+quaternionAngle = p.getQuaternionFromEuler([0, 0, -1])
+armID = p.loadURDF("./exo-urdf/01-exo_arm.urdf", basePosition = [0, 0, -2], baseOrientation = quaternionAngle, useFixedBase=True, globalScaling = 4)
 
 numJoints = p.getNumJoints(armID)
 #print("number of joints discovered on the robot is: " + str(numJoints))
@@ -25,15 +27,22 @@ jointType = elbowJointInfo[jointTypeIndex]
 jointLowerLimit = elbowJointInfo[jointLowerLimitIndex]
 jointUpperLimit = elbowJointInfo[jointUpperLimitIndex]
 
-#p.setJointMotorControl2(armID, elbowJointID, POSITION_CONTROL, targetPosition = 1.5)
+#p.setJointMotorControl2(armID, elbowJointID, p.POSITION_CONTROL, targetPosition = 1.5)
 
+test_data = []
+for i in range(20):
+	test_data.append(i * 0.1)
+print(test_data)
 
+data_rate = 1
 for i in range(10000):
-	pos = 2
-	p.setJointMotorControl2(armID, elbowJointID, p.POSITION_CONTROL, targetPosition = pos)	
+	if i < len(test_data):
+		pos = test_data[i]
+	p.setJointMotorControl2(armID, elbowJointID, p.POSITION_CONTROL, targetPosition = pos, positionGain = 1)	
 	print(p.getJointState(armID, elbowJointID)[0])
 	p.stepSimulation()
-	time.sleep(1/500)
+	#print("1s interval: " + str(time.time()))
+	time.sleep(data_rate)
 
 p.disconnect()
 	
